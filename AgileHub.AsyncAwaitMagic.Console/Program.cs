bd26623f499e79969ced69ece9bc9786058c5ebc
Doru Foaltin <dorin.foaltin@hotmail.com>
2019-09-24 18:40:31 +0300

    Add Exercise3

---
+++ AgileHub.AsyncAwaitMagic.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using AgileHub.AsyncAwaitMagic.Standard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileHub.AsyncAwaitMagic.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient restClient = new RestClient();
+
+            var result = restClient.Get("http://asyncawaitmagic.azurewebsites.net/api/demo").Result;
+
+            // Code reached
+            if (result.IsSuccessStatusCode)
+            {
+                // ...
+            }
+        }
+    }
+}
--- AgileHub.AsyncAwaitMagic.WPF/MainWindow.xaml.cs
+++ AgileHub.AsyncAwaitMagic.WPF/MainWindow.xaml.cs
@@ -25,17 +25,17 @@ namespace AgileHub.AsyncAwaitMagic.WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            RestClient restClient = new RestClient();
+
+            //var result = restClient.Get("http://asyncawaitmagic.azurewebsites.net/api/demo").Result;
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
             RestClient restClient = new RestClient();
 
-            // Console.WriteLine($"Current thread id is: { Thread.CurrentThread.ManagedThreadId }");
-
             var result = await restClient.Get("http://asyncawaitmagic.azurewebsites.net/api/demo");
-
-            // Console.WriteLine($"Continuation thread id is: { Thread.CurrentThread.ManagedThreadId }");
 
             resultlabel.Text = await result.Content.ReadAsStringAsync();
         }
